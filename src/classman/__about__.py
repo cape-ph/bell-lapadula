1f58f96cf43de4668a857b4e8a643f4fd5b6488a
True Merrill <true.merrill@gtri.gatech.edu>
2024-01-11 14:04:12 -0500

    Adding FastAPI prototype

---
+++ src/classman/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present True Merrill <true.merrill@gtri.gatech.edu>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/classman/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present True Merrill <true.merrill@gtri.gatech.edu>
+#
+# SPDX-License-Identifier: MIT
